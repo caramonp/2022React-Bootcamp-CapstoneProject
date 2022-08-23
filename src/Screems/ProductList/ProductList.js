3c1c874cad5ef34504fff702cd27e5886f1cf446
Carolina <carolinaramon239578@correo.itm.edu.co>
2022-08-23 09:03:30 -0500

    filter and side bar to filter proudcts for categories

--- src/Screems/ProductList/ProductList.js
+++ src/Screems/ProductList/ProductList.js
@@ -5,41 +5,37 @@ import CardFeatured from "../../components/Home/Session3Home/CardFeature/CardFea
 import "./ProductList.css";
 
 const ProductList = () => {
-  const [products, setProducts] = useState(data.results)
+  const [products, setProducts] = useState(data.results);
   const [checked, setChecked] = useState([]);
-
-
 
   const filter = () => {
     const filterProducts = data.results.filter((product) => {
       return checked.some((category) => {
         return (
-          category.toLowerCase() ===
-          product.data.category.slug.toLowerCase()
+          category.toLowerCase() === product.data.category.slug.toLowerCase()
         );
       });
-    })
-    return (
-      checked.length ? setProducts(filterProducts) : setProducts(data.results)
-      )
-  }
-
-  
+    });
+    return checked.length
+      ? setProducts(filterProducts)
+      : setProducts(data.results);
+  };
 
   return (
     <div className="product-list-container">
       <h1>Product List</h1>
       <div className="body-products">
-        <SideBar checked={checked} setChecked={setChecked} filter={filter}/>
+        <SideBar checked={checked} setChecked={setChecked} filter={filter} />
         <div>
-         {products.length? <CardFeatured
-            description={products}
-          /> : <h1>No existe</h1>}
+          {products.length ? (
+            <CardFeatured description={products} />
+          ) : (
+            <h1>No existe</h1>
+          )}
         </div>
       </div>
     </div>
-  )
-   
+  );
 };
 
 export default ProductList;
